6538208265b463e24df8fc338fb372842d8196ae
Jacob Marshall <git@jacobmarshall.co>
2016-01-28 21:36:43 +1300

    Initial commit

---
+++ src/main/java/io/github/jacobmarshall/censor/Censor.java
@@ -0,0 +1,12 @@
+package io.github.jacobmarshall.censor;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Censor extends JavaPlugin {
+
+    @Override
+    public void onEnable () {
+        getLogger().info("Starting Censor...");
+    }
+
+}
