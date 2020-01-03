371133fc2440786ee32e977d92e74bb0098b96cb
Behoston <mlegiecki@gmail.com>
2020-01-04 00:02:20 +0100

    Refacotr: Simplify WordFilter.

--- src/main/java/io/github/jacobmarshall/meloooncensor/filter/WordFilter.java
+++ src/main/java/io/github/jacobmarshall/meloooncensor/filter/WordFilter.java
@@ -2,34 +2,19 @@ package io.github.jacobmarshall.meloooncensor.filter;
 
 import io.github.jacobmarshall.meloooncensor.config.Configuration;
 
-import java.util.regex.Pattern;
-
 
 public class WordFilter extends ClassicFilter {
-    private static final Pattern WORD_SPLIT = Pattern.compile("\\s");
-
     public WordFilter(Configuration configuration) {
         super(configuration);
     }
 
     @Override
     protected boolean isCensoredWord(String word) {
-        return config.getCensor().contains(word);
+        return config.getCensor().contains(word.toLowerCase());
     }
 
     @Override
     protected boolean isIgnoredWord(String word) {
-        return config.getIgnore().contains(word);
-    }
-
-    @Override
-    public boolean violatesPolicy(String message) {
-        String[] words = WORD_SPLIT.split(message);
-        for (String word : words) {
-            if (config.getCensor().contains(word.toLowerCase())) {
-                return true;
-            }
-        }
-        return false;
+        return config.getIgnore().contains(word.toLowerCase());
     }
 }
